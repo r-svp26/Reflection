15f967b4fbf430b800bb58c78d0c6edb5e04e2ef
r-svp26 <riteshk6341@gmail.com>
2021-08-18 22:38:07 +0530

    [Ritesh] Add: Reflection is implemented for Customer class

---
+++ Reflection/Customer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Reflection
+{
+    /// <summary>
+    /// Customer is class in which properties, constructors and methods are declared.
+    /// </summary>
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        /// <summary>
+        /// parameterized constructor
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="name"></param>
+        public Customer(int id, string name)
+        {
+            this.Id = id;
+            this.Name = name;
+        }
+        /// <summary>
+        /// default constructor
+        /// </summary>
+        public Customer()
+        {
+            this.Id = -1;
+            this.Name = "";
+        }
+        /// <summary>
+        /// print customer id
+        /// </summary>
+        public void PrintId()
+        {
+            Console.WriteLine(this.Id);
+        }
+        /// <summary>
+        /// print customer name
+        /// </summary>
+        public void PrintName()
+        {
+            Console.WriteLine(this.Name);
+        }
+    }
+}
--- Reflection/Program.cs
+++ Reflection/Program.cs
@@ -1,12 +1,56 @@
 ﻿using System;
+using System.Reflection;
 
 namespace Reflection
 {
     class Program
     {
+        /// <summary>
+        /// display list of methods, propertys, constructors and class details by reflection
+        /// </summary>
+        public static void Invoke()
+        {
+            //// get the type of class using Type class static methed GetType.
+            Type type = Type.GetType("Reflection.Customer");
+            //// printing full name of the class.
+            Console.WriteLine("Full Name is {0}", type.FullName);
+            //// printing class name.
+            Console.WriteLine("Class Name is {0}", type.Name);
+
+            Console.WriteLine("Methods in Customer class");
+            //// MethodInfo is a class where methods of Customer class is stored in the form of array.
+            MethodInfo[] methods = type.GetMethods();
+            //// traversing the present in the method object.
+            foreach (MethodInfo method in methods)
+            {
+                Console.WriteLine(method.ReturnType.Name + " " + method.Name);
+            }
+
+            Console.WriteLine("Properties in Customer class");
+            //// PropertyInfo is a class where properties of Customer class is stored in the form of array.
+            PropertyInfo[] properties = type.GetProperties();
+            //// traversing the data present in the property object.
+            foreach (PropertyInfo property in properties)
+            {
+                Console.WriteLine(property.PropertyType.Name + " " + property.Name);
+            }
+
+            Console.WriteLine("Constructors in Customer class");
+            //// ConstructorInfo is a class where constructors of Customer class is stored in the form of array.
+            ConstructorInfo[] constructors= type.GetConstructors();
+            //// traversing the data present in the constructor object.
+            foreach ( ConstructorInfo constructor in constructors)
+            {
+                Console.WriteLine( constructor.ToString());
+            }
+        }
+        /// <summary>
+        /// main method
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Reflection Project");
+            Invoke();
         }
     }
 }
